86d014b1a12b667bf2c23be086dc553a7141e3fe
justinfleming <jfleming2@css.edu>
2020-09-24 10:39:50 -0500

    v2

--- unit3inclass/Program.cs
+++ unit3inclass/Program.cs
@@ -2,11 +2,31 @@
 
 namespace unit3inclass
 {
-    class Program
+    class rocket
     {
+        private String name;
+        private int model;
+
+        public rocket()
+        {
+            name = "";
+            model = 0;
+        }
+
+
+        public rocket(String nname, int nmodel)
+        {
+            model = nmodel;
+            name = nname;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi github");
         }
+
+
     }
 }
